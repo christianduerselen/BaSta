e280a30cd67d60c494add86a4a2f5cb27e657d16
Christian Dürselen <christian.duerselen@zeiss.com>
2022-01-18 08:48:55 +0100

    Protocol project stubs for Bodet and Wige.

---
+++ src/BaSta.Protocol.Bodet.Tests/Resources/EmbeddedResources.cs
@@ -0,0 +1,9 @@
+﻿namespace BaSta.Protocol.Bodet.Tests.Resources;
+
+/// <summary>
+/// Static class containing names of all available embedded resources of this assembly.
+/// </summary>
+internal static class EmbeddedResources
+{
+    internal const string TraceDump = "Bodet-Trace.log";
+}
