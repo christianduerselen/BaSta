b999d9e4a63b8b3f44a8c7c3af3d588be9076c1c
Christian Dürselen <17407360+christianduerselen@users.noreply.github.com>
2022-12-18 12:30:12 +0100

    General update and WIP for Stramatel Sound Board.

---
+++ src/BaSta.StramatelSoundBoard/LoopStream.cs
@@ -0,0 +1,59 @@
+﻿using NAudio.Wave;
+
+namespace BaSta.StramatelSoundBoard;
+
+internal class LoopStream : WaveStream
+{
+    readonly WaveStream _sourceStream;
+
+    /// <summary>
+    /// Creates a instance of the <see cref="LoopStream"/> class.
+    /// </summary>
+    /// <param name="sourceStream">The stream to read from. Note: the Read method of this stream should return 0 when it reaches the end or else we will not loop to the start again.</param>
+    public LoopStream(WaveStream sourceStream)
+    {
+        _sourceStream = sourceStream;
+        EnableLooping = true;
+    }
+
+    /// <summary>
+    /// Use this to turn looping on or off.
+    /// </summary>
+    public bool EnableLooping { get; set; }
+
+    /// <inheritdoc />
+    public override WaveFormat WaveFormat => _sourceStream.WaveFormat;
+
+    /// <inheritdoc />
+    public override long Length => _sourceStream.Length;
+
+    /// <inheritdoc />
+    public override long Position
+    {
+        get => _sourceStream.Position;
+        set => _sourceStream.Position = value;
+    }
+
+    /// <inheritdoc />
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        int totalBytesRead = 0;
+
+        while (totalBytesRead < count)
+        {
+            int bytesRead = _sourceStream.Read(buffer, offset + totalBytesRead, count - totalBytesRead);
+            if (bytesRead == 0)
+            {
+                if (_sourceStream.Position == 0 || !EnableLooping)
+                {
+                    // something wrong with the source stream
+                    break;
+                }
+                // loop
+                _sourceStream.Position = 0;
+            }
+            totalBytesRead += bytesRead;
+        }
+        return totalBytesRead;
+    }
+}
---
+++ src/BaSta.StramatelSoundBoard/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NAudio.Wave;
+
+namespace BaSta.StramatelSoundBoard;
+
+public static class Program
+{
+    public static void Main()
+    {
+        while (true)
+        {
+            IList<DirectSoundDeviceInfo> devices = DirectSoundOut.Devices.ToList();
+
+            Console.WriteLine("Select output device:");
+            for (int i = 1; i < devices.Count; i++)
+            {
+                Console.WriteLine($"> [{i}] {devices[i].Description} ({devices[i - 1].ModuleName})");
+            }
+
+            Console.Write("Index: ");
+            string index = Console.ReadLine();
+            int idx = int.Parse(index);
+            Guid deviceGuid = devices[idx - 1].Guid;
+
+            Console.WriteLine();
+            Console.WriteLine($"Starting playback on device \"{devices[idx-1].Description}\"!");
+
+            var audioFile = new AudioFileReader(@"C:\Users\Christian\Desktop\GameHornDefault_2.wav");
+            audioFile.Volume = 3f;
+            
+            using (var loopedAudioFile = new LoopStream(audioFile))
+            using (var outputDevice = new DirectSoundOut(deviceGuid))
+            {
+                outputDevice.Init(loopedAudioFile);
+                outputDevice.Play();
+                
+                Console.WriteLine("Press any key to stop playback...");
+                Console.ReadKey(true);
+                
+                outputDevice.Stop();
+            }
+        }
+    }
+}
